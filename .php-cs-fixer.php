62fc20ee3a6e922dae69ef89b10b4b88a219c3a0
Wong Hoi Sing Edison <hswong3i@gmail.com>
2016-05-26 17:05:07 +0800

    fix typo

---
+++ .php-cs-fixer.php
@@ -0,0 +1,30 @@
+<?php
+
+$fixers = [
+    '-no_empty_lines_after_phpdocs',
+    '-psr0',
+    'ordered_use',
+    'php_unit_construct',
+    'php_unit_strict',
+    'phpdoc_order',
+    'short_array_syntax',
+];
+
+$finder = Symfony\CS\Finder\DefaultFinder::create()
+    ->exclude('build')
+    ->exclude('vendor')
+    ->exclude('web')
+    ->ignoreDotFiles(false)
+    ->ignoreVCS(true)
+    ->in(__DIR__)
+    ->notName('*.phar')
+    ->notName('LICENSE')
+    ->notName('README.md')
+    ->notName('composer.*')
+    ->notName('phpunit.xml*');
+
+return Symfony\CS\Config\Config::create()
+    ->level(Symfony\CS\FixerInterface::SYMFONY_LEVEL)
+    ->setUsingCache(false)
+    ->fixers($fixers)
+    ->finder($finder);

361ed74658d71f22f7e4cf4782faa5083f14ff30
Wong Hoi Sing Edison <hswong3i@gmail.com>
2016-05-28 17:30:01 +0800

    initial commit for drupal/drupal:~7.43

---
+++ .php-cs-fixer.php
@@ -0,0 +1,29 @@
+<?php
+
+$fixers = [
+    '-no_empty_lines_after_phpdocs',
+    '-psr0',
+    'ordered_use',
+    'php_unit_construct',
+    'php_unit_strict',
+    'phpdoc_order',
+    'short_array_syntax',
+];
+
+$finder = Symfony\CS\Finder\DefaultFinder::create()
+    ->exclude('build')
+    ->exclude('vendor')
+    ->ignoreDotFiles(false)
+    ->ignoreVCS(true)
+    ->in(__DIR__.'/src')
+    ->notName('*.phar')
+    ->notName('LICENSE')
+    ->notName('README.md')
+    ->notName('composer.*')
+    ->notName('phpunit.xml*');
+
+return Symfony\CS\Config\Config::create()
+    ->level(Symfony\CS\FixerInterface::SYMFONY_LEVEL)
+    ->setUsingCache(false)
+    ->fixers($fixers)
+    ->finder($finder);
---
+++ README.md
@@ -0,0 +1,4 @@
+DruStack Standard Edition
+==========================
+
+Welcome to the DruStack Standard Edition - a fully-functional Drupal application that you can use as the skeleton for your new applications.

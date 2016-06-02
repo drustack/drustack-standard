6b7c01c757f151a590f9068848964d561e1874d4
Wong Hoi Sing Edison <hswong3i@gmail.com>
2016-06-02 09:54:29 +0800

    sync with develop

--- README.md
+++ README.md
@@ -1,4 +1,62 @@
 DruStack Standard Edition
-==========================
+=========================
+
+[![Build Status](https://travis-ci.org/drustack/drustack-standard.svg?branch=master)](https://travis-ci.org/drustack/drustack-standard)
+[![Latest Stable Version](https://poser.pugx.org/drustack/framework-standard-edition/v/stable.svg)](https://packagist.org/packages/drustack/framework-standard-edition)
+[![Total Downloads](https://poser.pugx.org/drustack/framework-standard-edition/downloads.svg)](https://packagist.org/packages/drustack/framework-standard-edition)
+[![License](https://poser.pugx.org/drustack/framework-standard-edition/license.svg)](https://packagist.org/packages/drustack/framework-standard-edition)
 
 Welcome to the DruStack Standard Edition - a fully-functional Drupal application that you can use as the skeleton for your new applications.
+
+This project template will managing your site dependencies with [Composer](https://getcomposer.org/). If you want to know how to use it as replacement for
+[Drush Make](https://github.com/drush-ops/drush/blob/master/docs/make.md) visit
+the [Documentation on drupal.org](https://www.drupal.org/node/2471553).
+
+Usage
+-----
+
+First you need to [install composer](https://getcomposer.org/doc/00-intro.md#installation-linux-unix-osx).
+
+After that you can create the project:
+
+    composer create-project --stability dev --no-interaction drustack/standard-framework-edition:7.43.x-dev drustack
+
+With `composer require ...` you can download new dependencies to your
+installation.
+
+    cd drustack
+    composer require drupal/devel:~7.0
+
+The `composer create-project` command passes ownership of all files to the
+project that is created. You should create a new git repository, and commit
+all files not excluded by the .gitignore file.
+
+What does the template do?
+--------------------------
+
+When installing the given `composer.json` some tasks are taken care of:
+
+-   Drupal will be installed in the `web`-directory.
+-   Autoloader is implemented to use the generated composer autoloader in `vendor/autoload.php`,
+    instead of the one provided by Drupal (`web/vendor/autoload.php`).
+-   Modules (packages of type `drupal-module`) will be placed in `web/modules/contrib/`
+-   Theme (packages of type `drupal-theme`) will be placed in `web/themes/contrib/`
+-   Profiles (packages of type `drupal-profile`) will be placed in `web/profiles/`
+-   Creates default writable versions of `settings.php` and `services.yml`.
+-   Creates `sites/default/files`-directory.
+-   Latest version of drush is installed locally for use at `vendor/bin/drush`.
+-   Latest version of DrupalConsole is installed locally for use at `vendor/bin/drupal`.
+
+License
+-------
+
+-   Code released under [MIT](https://github.com/drustack/drustack-standard/blob/master/LICENSE)
+-   Docs released under [CC BY 4.0](http://creativecommons.org/licenses/by/4.0/)
+
+Author Information
+------------------
+
+-   Wong Hoi Sing Edison
+    -   <a href="https://twitter.com/hswong3i" class="uri" class="uri">https://twitter.com/hswong3i</a>
+    -   <a href="https://github.com/hswong3i" class="uri" class="uri">https://github.com/hswong3i</a>
+
--- src/Composer/ScriptHandler.php
+++ src/Composer/ScriptHandler.php
@@ -2,11 +2,220 @@
 
 namespace DruStack\Standard\Composer;
 
+use Composer\Installer\PackageEvent;
 use Composer\Script\Event;
+use Composer\Semver\Constraint\Constraint;
+use Drupal\Component\PhpStorage\FileStorage;
 use Symfony\Component\Filesystem\Filesystem;
 
+/**
+ * Provides static functions for composer script events.
+ *
+ * @see https://getcomposer.org/doc/articles/scripts.md
+ */
 class ScriptHandler
 {
+    protected static $packageToCleanup = [
+        'behat/mink' => ['tests', 'driver-testsuite'],
+        'behat/mink-browserkit-driver' => ['tests'],
+        'behat/mink-goutte-driver' => ['tests'],
+        'doctrine/cache' => ['tests'],
+        'doctrine/collections' => ['tests'],
+        'doctrine/common' => ['tests'],
+        'doctrine/inflector' => ['tests'],
+        'doctrine/instantiator' => ['tests'],
+        'egulias/email-validator' => ['documentation', 'tests'],
+        'fabpot/goutte' => ['Goutte/Tests'],
+        'guzzlehttp/promises' => ['tests'],
+        'guzzlehttp/psr7' => ['tests'],
+        'jcalderonzumba/gastonjs' => ['docs', 'examples', 'tests'],
+        'jcalderonzumba/mink-phantomjs-driver' => ['tests'],
+        'masterminds/html5' => ['test'],
+        'mikey179/vfsStream' => ['src/test'],
+        'paragonie/random_compat' => ['tests'],
+        'phpdocumentor/reflection-docblock' => ['tests'],
+        'phpunit/php-code-coverage' => ['tests'],
+        'phpunit/php-timer' => ['tests'],
+        'phpunit/php-token-stream' => ['tests'],
+        'phpunit/phpunit' => ['tests'],
+        'phpunit/php-mock-objects' => ['tests'],
+        'sebastian/comparator' => ['tests'],
+        'sebastian/diff' => ['tests'],
+        'sebastian/environment' => ['tests'],
+        'sebastian/exporter' => ['tests'],
+        'sebastian/global-state' => ['tests'],
+        'sebastian/recursion-context' => ['tests'],
+        'stack/builder' => ['tests'],
+        'symfony/browser-kit' => ['Tests'],
+        'symfony/class-loader' => ['Tests'],
+        'symfony/console' => ['Tests'],
+        'symfony/css-selector' => ['Tests'],
+        'symfony/debug' => ['Tests'],
+        'symfony/dependency-injection' => ['Tests'],
+        'symfony/dom-crawler' => ['Tests'],
+        // @see \Drupal\Tests\Component\EventDispatcher\ContainerAwareEventDispatcherTest
+        // 'symfony/event-dispatcher' => ['Tests'],
+        'symfony/http-foundation' => ['Tests'],
+        'symfony/http-kernel' => ['Tests'],
+        'symfony/process' => ['Tests'],
+        'symfony/psr-http-message-bridge' => ['Tests'],
+        'symfony/routing' => ['Tests'],
+        'symfony/serializer' => ['Tests'],
+        'symfony/translation' => ['Tests'],
+        'symfony/validator' => ['Tests', 'Resources'],
+        'symfony/yaml' => ['Tests'],
+        'symfony-cmf/routing' => ['Test', 'Tests'],
+        'twig/twig' => ['doc', 'ext', 'test'],
+        ];
+
+    protected static function findPackageKey($package_name)
+    {
+        $package_key = null;
+        // In most cases the package name is already used as the array key.
+        if (isset(static::$packageToCleanup[$package_name])) {
+            $package_key = $package_name;
+        } else {
+            // Handle any mismatch in case between the package name and array key.
+            // For example, the array key 'mikey179/vfsStream' needs to be found
+            // when composer returns a package name of 'mikey179/vfsstream'.
+            foreach (static::$packageToCleanup as $key => $dirs) {
+                if (strtolower($key) === $package_name) {
+                    $package_key = $key;
+                    break;
+                }
+            }
+        }
+
+        return $package_key;
+    }
+
+    protected static function deleteRecursive($path)
+    {
+        if (is_file($path) || is_link($path)) {
+            return unlink($path);
+        }
+        $success = true;
+        $dir = dir($path);
+        while (($entry = $dir->read()) !== false) {
+            if ($entry == '.' || $entry == '..') {
+                continue;
+            }
+            $entry_path = $path.'/'.$entry;
+            $success = static::deleteRecursive($entry_path) && $success;
+        }
+        $dir->close();
+
+        return rmdir($path) && $success;
+    }
+
+    public static function preAutoloadDump(Event $event)
+    {
+        // We need the root package so we can add our classmaps to its loader.
+        $package = $event->getComposer()->getPackage();
+        // We need the local repository so that we can query and see if it's likely
+        // that our files are present there.
+        $repository = $event->getComposer()->getRepositoryManager()->getLocalRepository();
+        // This is, essentially, a null constraint. We only care whether the package
+        // is present in vendor/ yet, but findPackage() requires it.
+        $constraint = new Constraint('>', '');
+        // Check for our packages, and then optimize them if they're present.
+        if ($repository->findPackage('symfony/http-foundation', $constraint)) {
+            $autoload = $package->getAutoload();
+            $autoload['classmap'] = array_merge($autoload['classmap'], [
+                'vendor/symfony/http-foundation/Request.php',
+                'vendor/symfony/http-foundation/ParameterBag.php',
+                'vendor/symfony/http-foundation/FileBag.php',
+                'vendor/symfony/http-foundation/ServerBag.php',
+                'vendor/symfony/http-foundation/HeaderBag.php',
+                ]);
+            $package->setAutoload($autoload);
+        }
+        if ($repository->findPackage('symfony/http-kernel', $constraint)) {
+            $autoload = $package->getAutoload();
+            $autoload['classmap'] = array_merge($autoload['classmap'], [
+                'vendor/symfony/http-kernel/HttpKernel.php',
+                'vendor/symfony/http-kernel/HttpKernelInterface.php',
+                'vendor/symfony/http-kernel/TerminableInterface.php',
+                ]);
+            $package->setAutoload($autoload);
+        }
+    }
+
+    public static function ensureHtaccess(Event $event)
+    {
+
+        // The current working directory for composer scripts is where you run
+        // composer from.
+        $vendor_dir = $event->getComposer()->getConfig()->get('vendor-dir');
+
+        // Prevent access to vendor directory on Apache servers.
+        $htaccess_file = $vendor_dir.'/.htaccess';
+        if (!file_exists($htaccess_file)) {
+            file_put_contents($htaccess_file, FileStorage::htaccessLines(true)."\n");
+        }
+
+        // Prevent access to vendor directory on IIS servers.
+        $webconfig_file = $vendor_dir.'/web.config';
+        if (!file_exists($webconfig_file)) {
+            $lines = <<<EOT
+<configuration>
+  <system.webServer>
+    <authorization>
+      <deny users="*">
+    </authorization>
+  </system.webServer>
+</configuration>
+EOT;
+            file_put_contents($webconfig_file, $lines."\n");
+        }
+    }
+
+    public static function vendorTestCodeCleanup(PackageEvent $event)
+    {
+        $vendor_dir = $event->getComposer()->getConfig()->get('vendor-dir');
+        $io = $event->getIO();
+        $op = $event->getOperation();
+        if ($op->getJobType() == 'update') {
+            $package = $op->getTargetPackage();
+        } else {
+            $package = $op->getPackage();
+        }
+        $package_key = static::findPackageKey($package->getName());
+        $message = sprintf('    Processing <comment>%s</comment>', $package->getPrettyName());
+        if ($io->isVeryVerbose()) {
+            $io->write($message);
+        }
+        if ($package_key) {
+            foreach (static::$packageToCleanup[$package_key] as $path) {
+                $dir_to_remove = $vendor_dir.'/'.$package_key.'/'.$path;
+                $print_message = $io->isVeryVerbose();
+                if (is_dir($dir_to_remove)) {
+                    if (static::deleteRecursive($dir_to_remove)) {
+                        $message = sprintf("      <info>Removing directory '%s'</info>", $path);
+                    } else {
+                        // Always display a message if this fails as it means something has
+                        // gone wrong. Therefore the message has to include the package name
+                        // as the first informational message might not exist.
+                        $print_message = true;
+                        $message = sprintf("      <error>Failure removing directory '%s'</error> in package <comment>%s</comment>.", $path, $package->getPrettyName());
+                    }
+                } else {
+                    // If the package has changed or the --prefer-dist version does not
+                    // include the directory this is not an error.
+                    $message = sprintf("      Directory '%s' does not exist", $path);
+                }
+                if ($print_message) {
+                    $io->write($message);
+                }
+            }
+
+            if ($io->isVeryVerbose()) {
+                // Add a new line to separate this output from the next package.
+                $io->write('');
+            }
+        }
+    }
+
     public static function installRequirementsFile(Event $event)
     {
         $fs = new Filesystem();
@@ -19,12 +228,58 @@ class ScriptHandler
             $event->getIO()->write('Create a sites/default/settings.php file with chmod 0666');
         }
 
+        // Prepare the services file for installation
+        if (!$fs->exists($root.'/sites/default/services.yml')) {
+            $fs->copy($root.'/sites/default/default.services.yml', $root.'/sites/default/services.yml');
+            $fs->chmod($root.'/sites/default/services.yml', 0666);
+            $event->getIO()->write('Create a sites/default/services.yml file with chmod 0666');
+        }
+
         // Create the files directory with chmod 0777
         if (!$fs->exists($root.'/sites/default/files')) {
             $oldmask = umask(0);
             $fs->mkdir($root.'/sites/default/files', 0777);
             umask($oldmask);
             $event->getIO()->write('Create a sites/default/files directory with chmod 0777');
+        }
+    }
+
+    public static function generateInfoMetadata(PackageEvent $event)
+    {
+        $op = $event->getOperation();
+        $installation_manager = $event->getComposer()->getInstallationManager();
+
+        $package = $op->getJobType() == 'update' ? $op->getTargetPackage() : $op->getPackage();
+        if (in_array($package->getType(), ['drupal-profile', 'drupal-module', 'drupal-theme'])) {
+            $project = preg_replace('/^.*\//', '', $package->getName());
+            $version = preg_replace('/^dev-(.*)/', '$1-dev', $package->getPrettyVersion());
+            $core = preg_replace('/^([0-9]).*$/', '$1.x', $version);
+            $datestamp = preg_match('/-dev$/', $version) ? time() : $package->getReleaseDate()->getTimestamp();
+            $date = date('Y-m-d', $datestamp);
+
+            $fs = new Filesystem();
+            $install_path = $installation_manager->getInstallPath($package);
+            if ($fs->exists($info_file = $install_path.'/'.$project.'.info')) {
+                $info = <<<METADATA
+
+; Information add by composer on {$date}
+core = "{$core}"
+project = "{$project}"
+version = "{$version}"
+datestamp = "{$datestamp}"
+METADATA;
+                file_put_contents($info_file, $info, FILE_APPEND);
+            } elseif ($fs->exists($info_file = $install_path.'/'.$project.'.info.yml')) {
+                $info = <<<METADATA
+
+# Information add by composer on {$date}
+core: "{$core}"
+project: "{$project}"
+version: "{$version}"
+datestamp: "{$datestamp}"
+METADATA;
+                file_put_contents($info_file, $info, FILE_APPEND);
+            }
         }
     }
 }

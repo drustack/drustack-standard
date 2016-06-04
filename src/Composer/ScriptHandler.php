2bdfefd42464da19150ccdcde4085fd99aa85c4d
Wong Hoi Sing Edison <hswong3i@gmail.com>
2016-06-04 21:49:13 +0800

    code cleanup

--- src/Composer/ScriptHandler.php
+++ src/Composer/ScriptHandler.php
@@ -248,15 +248,23 @@ EOT;
         $root = getcwd().'/web';
 
         // Prepare the settings file for installation
-        if ($fs->exists($root.'/sites/default/default.settings.php') && !$fs->exists($root.'/sites/default/settings.php')) {
-            $fs->copy($root.'/sites/default/default.settings.php', $root.'/sites/default/settings.php');
+        if ($fs->exists($root.'/sites/default/default.settings.php')
+            && !$fs->exists($root.'/sites/default/settings.php')) {
+            $fs->copy(
+                $root.'/sites/default/default.settings.php',
+                $root.'/sites/default/settings.php'
+            );
             $fs->chmod($root.'/sites/default/settings.php', 0666);
             $event->getIO()->write('Create a sites/default/settings.php file with chmod 0666');
         }
 
         // Prepare the services file for installation
-        if ($fs->exists($root.'/sites/default/default.services.yml') && !$fs->exists($root.'/sites/default/services.yml')) {
-            $fs->copy($root.'/sites/default/default.services.yml', $root.'/sites/default/services.yml');
+        if ($fs->exists($root.'/sites/default/default.services.yml')
+            && !$fs->exists($root.'/sites/default/services.yml')) {
+            $fs->copy(
+                $root.'/sites/default/default.services.yml',
+                $root.'/sites/default/services.yml'
+            );
             $fs->chmod($root.'/sites/default/services.yml', 0666);
             $event->getIO()->write('Create a sites/default/services.yml file with chmod 0666');
         }
@@ -275,7 +283,9 @@ EOT;
         $op = $event->getOperation();
         $installation_manager = $event->getComposer()->getInstallationManager();
 
-        $package = $op->getJobType() == 'update' ? $op->getTargetPackage() : $op->getPackage();
+        $package = $op->getJobType() == 'update'
+            ? $op->getTargetPackage()
+            : $op->getPackage();
         $install_path = $installation_manager->getInstallPath($package);
 
         if (in_array($package->getType(), ['drupal-profile', 'drupal-module', 'drupal-theme'])) {

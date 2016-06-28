24bd27ddc6f353734d28414a0486a32a6bdf8800
Wong Hoi Sing Edison <hswong3i@gmail.com>
2016-06-28 12:13:23 +0800

    Issue #2755745 by hswong3i: Generate package meta data by Composer.php

--- src/Composer/ScriptHandler.php
+++ src/Composer/ScriptHandler.php
@@ -67,7 +67,7 @@ class ScriptHandler
         'symfony/yaml' => ['Tests'],
         'symfony-cmf/routing' => ['Test', 'Tests'],
         'twig/twig' => ['doc', 'ext', 'test'],
-    ];
+        ];
 
     /**
      * Add vendor classes to Composer's static classmap.
@@ -91,7 +91,7 @@ class ScriptHandler
                 'vendor/symfony/http-foundation/FileBag.php',
                 'vendor/symfony/http-foundation/ServerBag.php',
                 'vendor/symfony/http-foundation/HeaderBag.php',
-            ]);
+                ]);
             $package->setAutoload($autoload);
         }
         if ($repository->findPackage('symfony/http-kernel', $constraint)) {
@@ -100,7 +100,7 @@ class ScriptHandler
                 'vendor/symfony/http-kernel/HttpKernel.php',
                 'vendor/symfony/http-kernel/HttpKernelInterface.php',
                 'vendor/symfony/http-kernel/TerminableInterface.php',
-            ]);
+                ]);
             $package->setAutoload($autoload);
         }
     }
@@ -108,7 +108,7 @@ class ScriptHandler
     /**
      * Ensures that .htaccess and web.config files are present in Composer root.
      *
-     * @param \Composer\Script\Event $event
+     * @param \Composer\Script\Event $event A Event object to get the configured composer vendor directories from.
      */
     public static function ensureHtaccess(Event $event)
     {
@@ -228,9 +228,9 @@ EOT;
         if ($fs->exists($root.'/sites/default/default.settings.php')
             && !$fs->exists($root.'/sites/default/settings.php')) {
             $fs->copy(
-                $root.'/sites/default/default.settings.php',
-                $root.'/sites/default/settings.php'
-            );
+                    $root.'/sites/default/default.settings.php',
+                    $root.'/sites/default/settings.php'
+                );
             $fs->chmod($root.'/sites/default/settings.php', 0666);
             $event->getIO()->write('Create a sites/default/settings.php file with chmod 0666');
         }
@@ -239,9 +239,9 @@ EOT;
         if ($fs->exists($root.'/sites/default/default.services.yml')
             && !$fs->exists($root.'/sites/default/services.yml')) {
             $fs->copy(
-                $root.'/sites/default/default.services.yml',
-                $root.'/sites/default/services.yml'
-            );
+                    $root.'/sites/default/default.services.yml',
+                    $root.'/sites/default/services.yml'
+                );
             $fs->chmod($root.'/sites/default/services.yml', 0666);
             $event->getIO()->write('Create a sites/default/services.yml file with chmod 0666');
         }
@@ -270,103 +270,53 @@ EOT;
             : $op->getPackage();
         $install_path = $installation_manager->getInstallPath($package);
 
-        if (in_array($package->getType(), ['drupal-profile', 'drupal-module', 'drupal-theme'])) {
+        if (in_array($package->getType(), [
+            'drupal-profile',
+            'drupal-module',
+            'drupal-theme',
+            ])) {
             $project = preg_replace('/^.*\//', '', $package->getName());
             $version = preg_replace(
-                ['/^dev-(.*)/', '/^([0-9]*)\.([0-9]*\.[0-9]*)/'],
-                ['$1-dev', '$1.x-$2'],
-                $package->getPrettyVersion()
-            );
+                    ['/^dev-(.*)/', '/^([0-9]*)\.([0-9]*\.[0-9]*)/'],
+                    ['$1-dev', '$1.x-$2'],
+                    $package->getPrettyVersion()
+                );
             $branch = preg_replace('/^([0-9]*\.x-[0-9]*).*$/', '$1', $version);
             $datestamp = preg_match('/-dev$/', $version)
-                ? time()
-                : $package->getReleaseDate()->getTimestamp();
+                    ? time()
+                    : $package->getReleaseDate()->getTimestamp();
 
-            // Compute the rebuild version string for a project.
-            $version = static::computeRebuildVersion($install_path, $branch) ?: $version;
+                // Compute the rebuild version string for a project.
+                $version = static::computeRebuildVersion($install_path, $branch) ?: $version;
 
-            // Generate version information for `.info` files in ini format.
-            $finder = new Finder();
+                // Generate version information for `.info` files in ini format.
+                $finder = new Finder();
             $finder->in($install_path)
-                ->files()
-                ->name('*.info')
-                ->notContains('datestamp =');
+                    ->files()
+                    ->name('*.info')
+                    ->notContains('datestamp =');
             foreach ($finder as $file) {
                 file_put_contents(
-                    $file->getRealpath(),
-                    static::generateInfoIniMetadata($version, $project, $datestamp),
-                    FILE_APPEND
-                );
+                        $file->getRealpath(),
+                        static::generateInfoIniMetadata($version, $project, $datestamp),
+                        FILE_APPEND
+                    );
             }
 
-            // Generate version information for `.info.yml` files in YAML format.
-            $finder = new Finder();
+                // Generate version information for `.info.yml` files in YAML format.
+                $finder = new Finder();
             $finder->in($install_path)
-                ->files()
-                ->name('*.info.yml')
-                ->notContains('datestamp :');
+                    ->files()
+                    ->name('*.info.yml')
+                    ->notContains('datestamp :');
             foreach ($finder as $file) {
                 file_put_contents(
-                    $file->getRealpath(),
-                    static::generateInfoYamlMetadata($version, $project, $datestamp),
-                    FILE_APPEND
-                );
+                        $file->getRealpath(),
+                        static::generateInfoYamlMetadata($version, $project, $datestamp),
+                        FILE_APPEND
+                    );
             }
         }
-    }
-
-    /**
-     * Find the array key for a given package name with a case-insensitive search.
-     *
-     * @param string $package_name The package name from composer. This is always already lower case.
-     *
-     * @return null|string The string key, or NULL if none was found.
-     */
-    protected static function findPackageKey($package_name)
-    {
-        $package_key = null;
-        // In most cases the package name is already used as the array key.
-        if (isset(static::$packageToCleanup[$package_name])) {
-            $package_key = $package_name;
-        } else {
-            // Handle any mismatch in case between the package name and array key.
-            // For example, the array key 'mikey179/vfsStream' needs to be found
-            // when composer returns a package name of 'mikey179/vfsstream'.
-            foreach (static::$packageToCleanup as $key => $dirs) {
-                if (strtolower($key) === $package_name) {
-                    $package_key = $key;
-                    break;
-                }
-            }
-        }
-
-        return $package_key;
-    }
-
-    /**
-     * Helper method to remove directories and the files they contain.
-     *
-     * @param string $path The directory or file to remove. It must exist.
-     *
-     * @return bool TRUE on success or FALSE on failure.
-     */
-    protected static function deleteRecursive($path)
-    {
-        if (is_file($path) || is_link($path)) {
-            return unlink($path);
-        }
-        $success = true;
-        $dir = dir($path);
-        while (($entry = $dir->read()) !== false) {
-            if ($entry == '.' || $entry == '..') {
-                continue;
-            }
-            $entry_path = $path.'/'.$entry;
-            $success = static::deleteRecursive($entry_path) && $success;
-        }
-        $dir->close();
-
-        return rmdir($path) && $success;
     }
 
     /**
@@ -454,5 +404,62 @@ datestamp: "{$datestamp}"
 METADATA;
 
         return $info;
+    }
+
+    /**
+     * Find the array key for a given package name with a case-insensitive search.
+     *
+     * @param string $package_name The package name from composer. This is always already lower case.
+     *
+     * @return null|string The string key, or NULL if none was found.
+     */
+    protected static function findPackageKey($package_name)
+    {
+        $package_key = null;
+        // In most cases the package name is already used as the array key.
+        if (isset(static::$packageToCleanup[$package_name])) {
+            $package_key = $package_name;
+        } else {
+            // Handle any mismatch in case between the package name and array key.
+            // For example, the array key 'mikey179/vfsStream' needs to be found
+            // when composer returns a package name of 'mikey179/vfsstream'.
+            foreach (static::$packageToCleanup as $key => $dirs) {
+                if (strtolower($key) === $package_name) {
+                    $package_key = $key;
+                    break;
+                }
+            }
+        }
+
+        return $package_key;
+    }
+
+    /**
+     * Helper method to remove directories and the files they contain.
+     *
+     * @param string $path The directory or file to remove. It must exist.
+     *
+     * @return bool TRUE on success or FALSE on failure.
+     */
+    protected static function deleteRecursive($path)
+    {
+        if (is_file($path) || is_link($path)) {
+            return unlink($path);
+        }
+        $success = true;
+        // Avoid warnings when opendir does not have the permissions to open a
+        // directory.
+        if ($handle = @opendir($path)) {
+            while (false !== ($entry = readdir($handle))) {
+                if ($entry == '.' || $entry == '..') {
+                    continue;
+                }
+                $entry_path = $path.'/'.$entry;
+                $success = static::deleteRecursive($entry_path) && $success;
+            }
+            closedir($handle);
+
+            return rmdir($path) && $success;
+        }
     }
 }

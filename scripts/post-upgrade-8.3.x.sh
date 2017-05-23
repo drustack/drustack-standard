b25f0e0f21ada4ebecfff1c33c54efeec908a0a9
Wong Hoi Sing Edison <hswong3i@gmail.com>
2017-05-23 11:15:24 +0800

    add pre/post 8.3.x upgrade

---
+++ scripts/post-upgrade-8.3.x.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+for i in `seq 1 3`; do
+    composer update -n
+done
+
+cd web
+drush -y updatedb
+drush -y core-cron
+drush -y cache-rebuild
+drush -y pm-enable bootstrap_layouts content_moderation
